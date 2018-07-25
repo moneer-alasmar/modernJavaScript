956ce46cd790e52fe53adb42ed85f6454cab06fd
moneer-alasmar <kiraalasmar@gmail.com>
2018-07-25 11:03:51 -0700

    completed delete and filter tasks

--- domProjects/taskListPart2DeleteFilterTasks/app.js
+++ domProjects/taskListPart2DeleteFilterTasks/app.js
@@ -10,8 +10,18 @@ loadEventListeners();
 
 // LOAD ALL EVENT LISTENERS FUNCTION
 function loadEventListeners() {
-  // add task event
+  // ADD TASK EVENT
   form.addEventListener('submit', addTask);
+
+  // REMOVE TASK EVENT
+  taskList.addEventListener('click', removeTask);
+
+  // CLEAR ALL TASKS EVENT
+  clearBtn.addEventListener('click', clearTasks);
+
+  // FILTER TASKS EVENT
+  filter.addEventListener('keyup', filterTasks);
+
 }
 
 // ADD TASK FUNCTION
@@ -48,4 +58,41 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// REMOVE TASK FUNCTION
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure you want to delete this item?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// CLEAR TASKS FUNCTION
+function clearTasks() {
+  // INFERIOR METHOD
+  taskList.innerHTML = '';
+
+  // FASTER METHOD
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+  
+  // SUPPORTING DOCS FOR METHODS
+  // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// FILTER TASKS FUNCTION
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
