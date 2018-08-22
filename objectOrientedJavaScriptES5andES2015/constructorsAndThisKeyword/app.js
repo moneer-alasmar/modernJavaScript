dc3bc3d01e45df7b1f6a51b531cd11e2f0bdf2c4
moneer-alasmar <kiraalasmar@gmail.com>
2018-08-22 15:54:02 -0700

    completed this keyword

--- objectOrientedJavaScriptES5andES2015/constructorsAndThisKeyword/app.js
+++ objectOrientedJavaScriptES5andES2015/constructorsAndThisKeyword/app.js
@@ -1,4 +1,20 @@
 // PERSON CONSTRUCCTOR
-function Person() {
-  this.name = 'Moneer';
+function Person(name, dob) {
+  this.name = name;
+  // this.age = age;
+  this.birthday = new Date(dob);
+  this.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
 }
+
+// const moneer = new Person('Moneer', 31);
+// const mo = new Person('Meagan', 27);
+
+// console.log(moneer.age);
+
+const moneer = new Person('Moneer', '12-6-1986');
+
+console.log(moneer.calculateAge());
