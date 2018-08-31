3f1ac753c59d873192aa3cd9ad07519bccfaf49d
moneer-alasmar <kiraalasmar@gmail.com>
2018-08-31 10:19:39 -0700

    completed github finder

---
+++ asynchronousJavaScriptAjaxAndFetchApi/githubFinder/Part5FetchAndDisplayRepos/app.js
@@ -0,0 +1,32 @@
+// INIT GITHUB
+const github = new Github;
+
+// INIT UI
+const ui = new UI;
+
+// SEARCH INPUT
+const searchUser =  document.getElementById('searchUser');
+
+// SEARCH INPUT EVENT LISTENER
+searchUser.addEventListener('keyup', (e) => {
+  // GET INPUT TEXT
+  const userText = e.target.value;
+
+  if(userText != '') {
+    // MAKE HTTP CALL
+    github.getUser(userText)
+      .then(data => {
+        if(data.profile.message === 'Not Found') {
+          // SHOW ALERT
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          // SHOW PROFILE
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+  } else {
+    // CLEAR PROFILE
+    ui.clearProfile();
+  }
+});
